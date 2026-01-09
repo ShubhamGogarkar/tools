20d9c134c60df7b150ee1f7b5e51905508658c71
ShubhamGogarkar <shubhamgogarkar22@gmail.com>
2026-01-09 19:20:34 +0530

    added alarm mechanism

--- alarm.js
+++ alarm.js
@@ -1,14 +1,34 @@
+hourBox=document.querySelector('.hour');
+minuteBox=document.querySelector('.minute');
+secondBox=document.querySelector('.second');
+setHourBox=document.querySelector('.setHour');
+setMinuteBox=document.querySelector('.setMinute');
+setSecondBox=document.querySelector('.setSecond');
+
+
+
 function updateClock(){
 
 const now = new Date();
 const hours = now.getHours();
 const minutes = now.getMinutes();
 const seconds = now.getSeconds();
-document.querySelector('.hour').innerText = hours.toString().padStart(2, '0');
-document.querySelector('.minute').innerText = minutes.toString().padStart(2, '0');
-document.querySelector('.second').innerText = seconds.toString().padStart(2, '0');
+hourBox.innerText = hours;
+minuteBox.innerText = minutes;
+secondBox.innerText = seconds;
+
+
+
+setSec=Number(setSecondBox.value);
+setMin=Number(setMinuteBox.value);
+setHour=Number(setHourBox.value);
+
+
+if (hours === setHour && minutes === setMin && seconds === setSec)  {
+    console.log("Alarm ringing!");}
+
+
+
 }
 
-
-setInterval(updateClock, 1000);d
-
+setInterval(updateClock, 1000);
